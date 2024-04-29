a8d7fc5f82428b55f048b7cc72750ff0ee5e1367
Y-HuiMing-Y <119154148+Y-HuiMing-Y@users.noreply.github.com>
2024-04-29 23:29:13 +0800

    Update clip_pretrain.py

--- models/clip_pretrain.py
+++ models/clip_pretrain.py
@@ -57,12 +57,12 @@ class CLIP_Pretrain(nn.Module):
         self.visual_encoder, vision_width = create_vit(vit,image_size, vit_grad_ckpt, vit_ckpt_layer, 0)
         
         # vit模块的更改,text_encoder更改为bert-base-chinese
-        if vit=='deit_tiny_patch16_224':
+        if vit == 'base':
             checkpoint = torch.hub.load_state_dict_from_url(
-                url="https://dl.fbaipublicfiles.com/deit/deit_tiny_patch16_224-a1311bcf.pth",
+                url="https://dl.fbaipublicfiles.com/deit/deit_base_patch16_224-b5f2ef4d.pth",
                 map_location="cpu", check_hash=True)
-            state_dict = checkpoint["model"]     
-            msg = self.visual_encoder.load_state_dict(state_dict,strict=False)
+            state_dict = checkpoint["model"]
+            msg = self.visual_encoder.load_state_dict(state_dict, strict=False)
                
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-german-cased') #Download ‘bert-base-chinese’ in advance and save it to a local path 
         encoder_config = BertConfig.from_json_file(med_config)
